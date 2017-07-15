da67bff0daad2eae104d0f7f6fa8a5316d3b0246
tjkSec <sudozapto@gmail.com>
2017-07-15 12:32:57 +0800

    Added more methods and others to each regex.

--- src/syntack/syntax.js
+++ src/syntack/syntax.js
@@ -1,10 +1,10 @@
 export var parsedFunc = (__code__, __theme__) => {
   var strReg1 = /"(.*?)"/g,
     strReg2 = /'(.*?)'/g,
-    specialReg = /\b(new|const|class|let|var|if|do|function|while|switch|for|forEach|in|continue|break|return)(?=[^\w])/g,
-    specialJsGlobReg = /\b(document|window|Array|String|Object|Number|\$)(?=[^\w])/g,
-    specialJsReg = /\b(getElementsBy(TagName|ClassName|Name)|getElementById|typeof|instanceof|alert)(?=[^\w])/g,
-    specialMethReg = /\b(indexOf|match|split|splice|replace|toString|length|push|join|textContent)(?=[^\w])/g,
+    specialReg = /\b(function|break|const|let|var|switch|for|if|export|import|module.exports|else|delete|extends|super|constructor|catch|class|Class|catch|default|do|new|return|yeild|with|while|void|throw|try|while|finally|in|public|static|setInterval|setTimeout|clearInterval|RegExp)(?=[^\w])/g,
+    specialJsGlobReg = /\b(document|Promise|window|Array|console|Object|Boolean|null|undefined|Number|String|Symbol|Int8Array|Uint8Array|Uint8ClampedArray|Int16Array|Uint16Array|Int32Array|Uint32Array|Float32Array|Float64Array|this|\$)(?=[^\w])/g,
+    specialJsReg = /\b(getElementsBy(TagName|ClassName|Name|Id)|instanceof|alert|log|warn|querySelector|JSON|stringify)(?=[^\w])/g,
+    specialMethReg = /\b(indexOf|match|split|splice|replace|toString|length|push|join|textContent|unshift|concat|abs|acos|acosh|add|addSaturate|allTrue|apply|asin|big|bind|blink|bold|call|cbrt|ceil|charAt|charCodeAt|check|clear|compile|construct|copy|copyWithin|cosh|cos|create|defineProperties|defineProperty|div|entries|map|filter|fill|get|set|getDay|getDate|getFloat32|getFullYear|getHours|getInt16)(?=[^\w])/g,
     specialCommentReg = /(\/\*.*\*\/)/g,
     inlineCommentReg = /(\/\/.*)/g;
   /* BUG: Highlights the numbers within the <span/> style.
